4744bf7f28a5fdcf84f0a6730f4558ce5db66203
charludo <github@charlotteharludo.com>
2024-08-27 19:27:55 +0200

    Network visualization

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,13 +1,35 @@
+use bevy::prelude::*;
+use bevy_prototype_lyon::prelude::*;
 mod camera;
 mod network;
 
 use camera::CameraPlugin;
 use network::NetworkPlugin;
 
-use bevy::{prelude::*, window::PresentMode};
+use bevy::window::PresentMode;
 use bevy_mod_picking::{debug::DebugPickingPlugin, DefaultPickingPlugins, PickableBundle};
+use robmcf_greedy::Network;
+
+#[derive(Resource)]
+struct NetworkWrapper {
+    n: Network,
+}
 
 fn main() {
+    let network = NetworkWrapper {
+        n: Network::from_random(
+            20,       // num_vertices,
+            0.1,      // connectedness,
+            0.3,      // supply_density,
+            2,        // num_scenarios,
+            (3, 8),   // range_supply,
+            (15, 40), // range_capacity,
+            (4, 8),   // range_cost,
+            5,        // num_fixed_arcs,
+        ),
+    };
+    println!("{}", network.n);
+
     let height: f32 = 900.0;
     let resolution: f32 = 16.0 / 9.0;
     let width: f32 = height * resolution;
@@ -25,6 +47,8 @@ fn main() {
             ..default()
         }))
         .insert_resource(ClearColor(Color::srgb(0.16, 0.16, 0.18)))
+        .insert_resource(network)
+        .add_plugins(ShapePlugin)
         .add_plugins(CameraPlugin)
         .add_plugins(NetworkPlugin)
         .add_plugins(
--- gui/src/network/mod.rs
+++ gui/src/network/mod.rs
@@ -1,17 +1,19 @@
-use robmcf_greedy::Network;
+use bevy::prelude::*;
+use bevy::sprite::MaterialMesh2dBundle;
+use bevy_prototype_lyon::prelude::*;
 
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use crate::NetworkWrapper;
 
 pub struct NetworkPlugin;
 impl Plugin for NetworkPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_vertices);
-        // .add_systems(Update, ());
+        app.add_systems(Startup, (spawn_vertices, draw_arcs));
+        // .add_systems(Update, draw_arcs);
     }
 }
 
 #[derive(Component)]
-struct Vertex {}
+struct Vertex(usize);
 
 #[derive(Bundle)]
 struct VertexBundle {
@@ -23,22 +25,12 @@ fn spawn_vertices(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    network: Res<NetworkWrapper>,
 ) {
-    let n = Network::from_random(
-        20,       // num_vertices,
-        0.6,      // connectedness,
-        0.3,      // supply_density,
-        2,        // num_scenarios,
-        (3, 8),   // range_supply,
-        (15, 40), // range_capacity,
-        (4, 8),   // range_cost,
-        5,        // num_fixed_arcs,
-    );
-
-    for vertex in n.vertices {
+    for (i, vertex) in network.n.vertices.iter().enumerate() {
         let entity = commands
             .spawn(VertexBundle {
-                vertex: Vertex {},
+                vertex: Vertex(i),
                 mesh: MaterialMesh2dBundle {
                     mesh: meshes.add(Circle::new(100.)).into(),
                     material: materials.add(ColorMaterial::from(Color::WHITE)),
@@ -57,7 +49,7 @@ fn spawn_vertices(
             });
             parent.spawn(Text2dBundle {
                 text: Text::from_section(
-                    vertex.name,
+                    vertex.name.clone(),
                     TextStyle {
                         font_size: 50.0,
                         ..default()
@@ -67,5 +59,87 @@ fn spawn_vertices(
                 ..default()
             });
         });
+    }
+}
+
+#[derive(Component, Copy, Clone)]
+struct Arc {
+    s: usize,
+    t: usize,
+    s_pos: Vec2,
+    t_pos: Vec2,
+    capacity: usize,
+    cost: usize,
+    load: usize,
+}
+
+impl Arc {
+    pub fn arc_point(&self) -> Vec2 {
+        let midpoint = self.s_pos.midpoint(self.t_pos);
+        let ortho_offshoot = Vec2::new(0., -1.).rotate(self.s_pos - self.t_pos) * 0.2;
+        midpoint + ortho_offshoot
+    }
+}
+
+fn draw_arcs(mut commands: Commands, network: Res<NetworkWrapper>) {
+    let capacities = network.n.capacities.as_rows();
+    let costs = network.n.costs.as_rows();
+    for s in 0..network.n.vertices.len() {
+        let this_vertex = &network.n.vertices[s];
+        for (t, capacity) in capacities[s].iter().enumerate() {
+            if *capacity > 0 {
+                let other_vertex = &network.n.vertices[t];
+                let arc = Arc {
+                    s,
+                    t,
+                    s_pos: Vec2::new(this_vertex.x, this_vertex.y),
+                    t_pos: Vec2::new(other_vertex.x, other_vertex.y),
+                    capacity: capacities[s][t],
+                    cost: costs[s][t],
+                    load: 0,
+                };
+                let mut path_builder = PathBuilder::new();
+                path_builder.move_to(arc.s_pos);
+                path_builder.quadratic_bezier_to(arc.arc_point(), arc.t_pos);
+                let path = path_builder.build();
+
+                let entity = commands
+                    .spawn((
+                        ShapeBundle { path, ..default() },
+                        Stroke::new(Color::WHITE, 10.0),
+                        arc,
+                    ))
+                    .id();
+
+                let shape = shapes::RegularPolygon {
+                    sides: 3,
+                    feature: shapes::RegularPolygonFeature::Radius(12.0),
+                    ..shapes::RegularPolygon::default()
+                };
+                commands.entity(entity).with_children(|parent| {
+                    parent.spawn((
+                        ShapeBundle {
+                            path: GeometryBuilder::build_as(&shape),
+                            spatial: SpatialBundle {
+                                transform: Transform {
+                                    translation: (arc.s_pos.midpoint(arc.t_pos)
+                                        + Vec2::new(0., -1.).rotate(arc.s_pos - arc.t_pos) * 0.1)
+                                        .extend(0.),
+                                    rotation: Quat::from_axis_angle(
+                                        Vec3::new(0., 0., 1.),
+                                        Vec2::new(0., -1.).angle_between(arc.s_pos - arc.t_pos),
+                                    ),
+                                    ..default()
+                                },
+                                ..default()
+                            },
+                            ..default()
+                        },
+                        Stroke::new(Color::WHITE, 10.0),
+                        Fill::color(Color::WHITE),
+                    ));
+                });
+            }
+        }
     }
 }
